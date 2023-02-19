85839e6887690de92ee9308d1319903ea349a727
NatMaquieira <122941342+NatMaquieira@users.noreply.github.com>
2023-02-19 19:16:06 -0300

    Update ProductosVendidosControllers.cs
    Se guarda el get

--- Controllers/ProductosVendidosControllers.cs
+++ Controllers/ProductosVendidosControllers.cs
@@ -1,1 +1,18 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SistemaGestion.Modelos;
+using SistemaGestion.Repositorio;
 
+namespace SistemaGestion.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductosVendidosController : ControllerBase
+    {
+        [HttpGet("{idUsuario}")]
+        public ProductoVendido Obtener(int idUsuario)
+        {
+            return ManejadorProductoVendido.obtenerProductosVendidos(idUsuario);
+        }
+    }
+}
