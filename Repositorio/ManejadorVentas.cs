7de2cd3a4bc48374f3245894a5ed196a39959520
NatMaquieira <122941342+NatMaquieira@users.noreply.github.com>
2023-02-19 19:01:17 -0300

    Create ManejadorVentas.cs

---
+++ Repositorio/ManejadorVentas.cs
@@ -0,0 +1,29 @@
+{
+    internal class manejadorVentas
+    {
+        public static string cadenaConexion = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=SistemaGestion;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public List<Venta> obtenerVentas(int idUsuario)
+        {
+            List<Venta> ventas = new List<Venta>();
+            using (SqlConnection conn = new SqlConnection(cadenaConexion))
+            {
+                SqlCommand comando = new SqlCommand($"select * from Ventas where IdUsuario = '{idUsuario}'", conn);
+                conn.Open();
+                SqlDataReader reader = comando.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Venta ventaTemporal = new Venta();
+                        ventaTemporal.Id = reader.GetInt64(0);
+                        ventaTemporal.Comentario = reader.GetString(1);
+                        ventaTemporal.IdUsuario = reader.GetInt64(2);
+                        
+
+                        ventas.Add(ventaTemporal);
+                    }
+                }
+                return ventas;
+            }
+        }
+}
