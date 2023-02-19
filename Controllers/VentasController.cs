91be2209d6e2eb14aec2f9ef14f154f2c6d996c2
NatMaquieira <122941342+NatMaquieira@users.noreply.github.com>
2023-02-19 19:17:38 -0300

    Update VentasController.cs

--- Controllers/VentasController.cs
+++ Controllers/VentasController.cs
@@ -1,1 +1,18 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SistemaGestion.Modelos;
+using SistemaGestion.Repositorio;
 
+namespace SistemaGestion.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VentasController : ControllerBase
+    {
+        [HttpGet("{idUsuario}")]
+        public Ventas Obtener(int idUsuario)
+        {
+            return ManejadorVentas.obtenerVentas(idUsuario);
+        }
+    }
+}
